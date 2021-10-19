da4fe2ba22a41ba9bf99977a1bfeef9fab294204
Preaditor <adamdilger@virginmedia.com>
2021-10-19 19:19:53 +0100

    Login & Signup
    Created Signup page, Login page now has both button working

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,48 +1,77 @@
 <template>
-    <div id="login_form">
-        <form class="form" id="form_pt1" action="">
-        <h1 id="title-main">Ready to continue?</h1>
-        <input
-          id="input-u"
-          class="user_info"
-          type="text"
-          name="email"
-          value=""
-          placeholder="Email"
-        />
-        <input
-          id="input-p"
-          class="user_info"
-          type="password"
-          name="pass"
-          value=""
-          placeholder="Password"
-        />
-        <div id="remember_info">
-          <label>Remember me</label>
-          <input id="remember" type="checkbox" value="" />
-        </div>
-        <p
-          style="
-            color: rgb(0, 0, 0);
-            margin-top: 5px;
-            padding-bottom: 0;
-            width: 500px;
-          "
-        >
-          Don't have an account?
-          <a href="../SignUp/index.php">Join Today!</a>
-        </p>
-        <p style="color: red; padding-bottom: 0; width: 500px"></p>
+  <div id="login_form">
+    <form class="form" id="form_pt1" @submit.prevent="Login">
+      <h1 id="title-main">Ready to continue?</h1>
+      <input
+        id="input-u"
+        class="user_info"
+        type="text"
+        name="email"
+        placeholder="Email"
+        v-model="email"
+      />
+      <input
+        id="input-p"
+        class="user_info"
+        type="password"
+        name="pass"
+        placeholder="Password"
+        v-model="password"
+      />
+      <div id="remember_info">
+        <label>Remember me</label>
+        <input id="remember" type="checkbox" value="" v-model="remember" />
+      </div>
+      <p
+        style="
+          color: rgb(0, 0, 0);
+          margin-top: 5px;
+          padding-bottom: 0;
+          width: 500px;
+        "
+      >
+        Don't have an account?
+        <button name="signup" id="Signup" type="button" v-on:click="Signup">Sign Up! </button>
+      </p>
+      <p style="color: red; padding-bottom: 0; width: 500px"></p>
 
-        <button name="login" id="input-submit" type="button" v-on:click="Login()" >
-          Login
-        </button>
-      </form>
-    </div>
+      <button name="login" id="input-submit" type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
+export default {
+  name: 'user-login',
+  data() {
+    return {
+      email: '',
+      email_valud: true,
+      email_pattern: new RegExp(/^\S+@\S+(\.\S+)+$/),
+      password: '',
+      remember: false,
+    };
+  },
+  computed: {
+    email_check() {
+      if (!this.email) {
+        return '';
+      }
+
+      return this.email_pattern.test(this.email)
+        ? console.log('input--valid')
+        : console.log('input--invalid');
+    },
+  },
+  methods: {
+    Login() {
+      console.log('works');
+    },
+    Signup() {
+      console.log('signup');
+    },
+  },
+};
 </script>
 
 <style lang="less">
@@ -92,6 +121,19 @@ input {
   height: 45px;
   border-radius: 30px;
   width: 15vw;
+  text-transform: uppercase;
+  color: white;
+  border: none;
+  outline: none;
+  text-decoration: none;
+  background-color: #3f436e;
+  margin-top: 20px;
+  transition: 0.5s all;
+}
+#Signup {
+      height: 45px;
+  border-radius: 30px;
+  width: 5vw;
   text-transform: uppercase;
   color: white;
   border: none;
