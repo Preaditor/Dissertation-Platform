68bd9c330c2ffa1035e43e85c76db784e1fb40ab
Preaditor <adamdilger@virginmedia.com>
2021-10-24 21:27:08 +0100

    Renaming
    Changed Lib -> lib
    Changed vuestates.js -> VueStates.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
-import VueStates from '@/Lib/vuestates';
+import VueStates from '@/lib/VueStates';
 
 const app = createApp(App);
 const appState = new VueStates();
