1a22a647e2dcf115bcd2be7c934a3f1d3e65a1ae
Preaditor <adamdilger@virginmedia.com>
2021-10-30 14:13:49 +0100

    Creating Dynamic Modules
    Started making modules load
    issue with images for modules not being loaded

--- src/components/Modules.vue
+++ src/components/Modules.vue
@@ -1,28 +1,50 @@
 <template>
-    <div id="module_container">
+    <div id="module_container" ref="modules">
+      <div
+      v-for="module in modules"
+      class="module"
+      :key="module.id"
+      >
+        <img :src="module.image">
+        <label class="title">{{ module.title }}</label>
+        <label class="description">{{ module.desc }}</label>
+      </div>
     </div>
 </template>
 
 <script>
-const modules = [
-  [
-    'Phishing',
-    '@/res/fontawesome/svgs/solid/share-nodes.svg',
-    'Phishing()',
-  ],
-  [
-    'Ransomware',
-    '@/res/fontawesome/svgs/solid/share-nodes.svg',
-    'Ransom()',
-  ],
-  [
-    'Social Engineering',
-    '@/res/fontawesome/svgs/solid/share-nodes.svg',
-    'Social()',
-  ],
-];
-console.log(modules);
-
+export default {
+  name: 'Modules',
+  data() {
+    return {
+      modules: [
+        {
+          id: 0,
+          title: 'Phishing',
+          desc: 'test test test',
+          active: false,
+          image: '@/res/fontawesome/svgs/solid/share-nodes.svg',
+        },
+        {
+          id: 1,
+          title: 'Ransom',
+          desc: 'test test test',
+          active: false,
+          image: '@/res/fontawesome/svgs/solid/share-nodes.svg',
+        },
+        {
+          id: 2,
+          title: 'Social',
+          desc: 'test test test',
+          active: false,
+          image: '@/res/fontawesome/svgs/solid/share-nodes.svg',
+        },
+      ],
+    };
+  },
+  methods: {
+  },
+};
 </script>
 
 <style lang="less">
