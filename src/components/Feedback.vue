c7d35ea7a248f1633a04b9fdf3020f18fe823269
Preaditor <adamdilger@virginmedia.com>
2022-04-10 22:44:53 +0100

    Create Feedback.vue

---
+++ src/components/Feedback.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="feedback">
+        <form class="form" id="module_feedback" @submit.prevent="submit">
+            <input
+                id="interface"
+                class="feedback_info"
+                type="slider"
+                name="interface"
+            />
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
