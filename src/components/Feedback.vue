980653a1f22af205d434164916d47c1740032afc
Preaditor <adamdilger@virginmedia.com>
2022-04-11 15:53:29 +0100

    Added more elements to feedback form

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -4,8 +4,60 @@
             <input
                 id="interface"
                 class="feedback_info"
-                type="slider"
+                type="range"
                 name="interface"
+                min="1"
+                max="10"
+                v-model="inter_value"
+                value="5"
+            />
+            <input
+                id="content"
+                class="feedback_info"
+                type="range"
+                name="content"
+                min="1"
+                max="10"
+                v-model="content_value"
+                value="5"
+            />
+            <input
+                id="design"
+                class="feedback_info"
+                type="range"
+                name="design"
+                min="1"
+                max="10"
+                v-model="design_value"
+                value="5"
+            />
+            <input
+                id="usability"
+                class="feedback_info"
+                type="range"
+                name="usability"
+                min="1"
+                max="10"
+                v-model="usability_value"
+                value="5"
+            />
+            <input
+                id="overall"
+                class="feedback_info"
+                type="range"
+                name="overall"
+                min="1"
+                max="10"
+                v-model="overall_value"
+                value="5"
+            />
+            <input
+                id="comment"
+                class="feedback_info"
+                type="text"
+                name="comment"
+                placeholder="Comment"
+                v-model="comment"
             />
 
         </form>
@@ -14,6 +66,16 @@
 
 <script>
 export default {
-    
+    name: 'feedback',
+    data() {
+        return {
+            inter_value: 5,
+            content_value: 5,
+            design_value: 5,
+            usability_value: 5,
+            overall_value: 5,
+            comment: '',
+        };
+    },
 }
 </script>
