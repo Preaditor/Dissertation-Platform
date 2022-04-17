cf526d26cc6bf7f4dc1d8236690d67e54576e3bb
Preaditor <adamdilger@virginmedia.com>
2022-04-17 02:10:19 +0100

    module content layout
    Starting layout setup

--- src/components/Module.vue
+++ src/components/Module.vue
@@ -8,6 +8,10 @@
           @click="intro($event)">{{ module.title }}</button>
     </div>
     <div id="interaction">
+      <p class="title"> Title</p>
+      <h1 class="detail"> Detial</h1>
+      <img class="image_1" src="https://blog.malwarebytes.com/wp-content/uploads/2018/09/shutterstock_749866270-900x506.jpg"/>
+      <img class="image_2" src="https://blog.malwarebytes.com/wp-content/uploads/2018/09/shutterstock_749866270-900x506.jpg"/>
     </div>
   </div>
 </template>
@@ -44,19 +48,34 @@ export default {
         },
         {
           id: 3,
-          title: 'Social',
-          desc: 'test test test',
-          active: false,
-          image: 'data:image/svg+xml;utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.0.0-beta2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M448 384c0 53.02-42.98 96-96 96s-96-42.98-96-96c0-3.984 .7012-7.776 1.174-11.63l-94.68-47.34C145.2 341.7 121.9 352 96 352c-53.02 0-96-42.98-96-96s42.98-96 96-96c25.86 0 49.23 10.34 66.5 26.97l94.68-47.34C256.7 135.8 256 131.1 256 128c0-53.02 42.98-96 96-96s96 42.98 96 96s-42.98 96-96 96c-25.86 0-49.23-10.34-66.5-26.97l-94.68 47.34C191.3 248.2 192 252 192 256S191.3 263.8 190.8 267.6l94.68 47.34C302.8 298.3 326.1 288 352 288C405 288 448 330.1 448 384z"/></svg>',
-          func: 'intro',
-        },
-        {
-          id: 4,
           title: 'test',
           desc: 'test test test',
           active: false,
           image: 'data:image/svg+xml;utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.0.0-beta2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M448 384c0 53.02-42.98 96-96 96s-96-42.98-96-96c0-3.984 .7012-7.776 1.174-11.63l-94.68-47.34C145.2 341.7 121.9 352 96 352c-53.02 0-96-42.98-96-96s42.98-96 96-96c25.86 0 49.23 10.34 66.5 26.97l94.68-47.34C256.7 135.8 256 131.1 256 128c0-53.02 42.98-96 96-96s96 42.98 96 96s-42.98 96-96 96c-25.86 0-49.23-10.34-66.5-26.97l-94.68 47.34C191.3 248.2 192 252 192 256S191.3 263.8 190.8 267.6l94.68 47.34C302.8 298.3 326.1 288 352 288C405 288 448 330.1 448 384z"/></svg>',
           func: 'intro',
+        },
+      ],
+      content: [
+        {
+          title: 'Phishing',
+          overview: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+          detail: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+          image_1: 'https://blog.malwarebytes.com/wp-content/uploads/2018/09/shutterstock_749866270-900x506.jpg',
+          image_2: 'https://www.kaspersky.com/content/en-global/images/repository/isc/2017-images/spam-phishing-img-02.jpg',
+        },
+        {
+          title: 'Ransom',
+          overview: '',
+          detail: '',
+          image_1: '',
+          image_2: '',
+        },
+        {
+          title: 'Social',
+          overview: '',
+          detail: '',
+          image_1: '',
+          image_2: '',
         },
       ],
     };
@@ -66,13 +85,17 @@ export default {
       console.log(e.value);
       console.log(e);
       console.log(e.target.innerHTML);
+      console.log(e.target.id);
       console.log('test');
-      let selected = e.target.innerHTML;
-      selected = selected.toLowerCase();
-      if (selected === 'phishing') {
+      const selected = e.target.id;
+      if (selected === '0') {
         console.log('selected section: phishing');
-      } else if (selected === 'ransom') {
+      } else if (selected === '1') {
         console.log('selected section: ransom');
+      } else if (selected === '2') {
+        console.log('Selected section: Social');
+      } else if (selected === '3') {
+        console.log('Selected section: test');
       }
     },
   },
@@ -80,7 +103,8 @@ export default {
 </script>
 
 <style lang="less">
-#nav {
+#module_interact {
+  > #nav {
     padding-top: 2%;
     display: flex;
     flex-direction: row;
@@ -88,7 +112,7 @@ export default {
     align-items: center;
     width: 100%;
     height: 100%;
-    .nav_button {
+    > .nav_button {
             height: 45px;
         border-radius: 30px;
         width: 15vw;
@@ -101,5 +125,37 @@ export default {
         margin-top: 20px;
         transition: 0.5s all;
     }
+  }
+  > #interaction {
+      margin: auto;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr 1fr;
+      gap: 0px 0px;
+      grid-template-areas:
+      ". ."
+      ". ."
+      ". .";
+      width: 80%;
+      height: 80%;
+      border-color: black;
+      border-style: solid;
+      border-width: 3px;
+      > .title {
+        grid-row: 1;
+        text-align: left;
+      }
+      > .detial {
+        grid-row: 2;
+        float: left;
+      }
+      > .image_1 {
+        grid-row: 2;
+        float: right;
+      }
+      > .image_2 {
+
+      }
+  }
 }
 </style>
